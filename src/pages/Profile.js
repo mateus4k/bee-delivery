692b0e308d7e13761ff7f351a05b24db9e157328
Mateus <mateus4k@protonmail.ch>
2019-10-31 23:49:09 -0300

    Applied componentization

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,17 +2,15 @@ import React, { useState, useEffect } from 'react'
 import {
   View,
   Text,
-  SafeAreaView,
   StyleSheet,
-  StatusBar,
   Platform,
   Image,
   TouchableOpacity,
-  ScrollView,
   AsyncStorage
 } from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons'
 import ShimmerPlaceHolder from 'react-native-shimmer-placeholder'
+import MainContainer from '../components/MainContainer'
 
 export default function Profile({ navigation }) {
   const [points, setPoints] = useState(0)
@@ -41,179 +39,145 @@ export default function Profile({ navigation }) {
   }, [])
 
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar translucent backgroundColor={'#202020'} />
-      <ScrollView style={styles.mainContainer}>
-        <View style={styles.headerContainer}>
-          <Text style={styles.headerText}>Perfil</Text>
-        </View>
+    <MainContainer name="Perfil">
+      <View style={styles.userContainer}>
+        <ShimmerPlaceHolder
+          style={styles.userImage}
+          visible={isFetched}
+          backgroundColorBehindBorder={'#ddd'}
+        >
+          <Image
+            style={styles.userImage}
+            source={isFetched ? { uri: userImage } : null}
+          />
+        </ShimmerPlaceHolder>
 
-        <View style={styles.mainContainer}>
-          <View style={styles.userContainer}>
-            <ShimmerPlaceHolder
-              style={styles.userImage}
-              visible={isFetched}
-              backgroundColorBehindBorder={'#ddd'}
-            >
-              <Image
-                style={styles.userImage}
-                source={isFetched ? { uri: userImage } : null}
+        <View style={styles.userInfoContainer}>
+          <ShimmerPlaceHolder
+            autoRun={true}
+            style={styles.userInfoName}
+            visible={isFetched}
+            backgroundColorBehindBorder={'#ddd'}
+          >
+            <Text style={styles.userInfoName}>{username}</Text>
+          </ShimmerPlaceHolder>
+          <View style={styles.userInfoList}>
+            <View style={styles.userInfoStats}>
+              <Icon
+                name={Platform.OS === 'ios' ? 'ios-star' : 'md-star'}
+                color="#555"
+                size={20}
+                style={styles.userInfoIcon}
               />
-            </ShimmerPlaceHolder>
-
-            <View style={styles.userInfoContainer}>
               <ShimmerPlaceHolder
-                autoRun={true}
-                style={styles.userInfoName}
+                style={{ width: 30 }}
                 visible={isFetched}
                 backgroundColorBehindBorder={'#ddd'}
               >
-                <Text style={styles.userInfoName}>{username}</Text>
+                <Text style={styles.userInfoText}>{stars}</Text>
               </ShimmerPlaceHolder>
-              <View style={styles.userInfoList}>
-                <View style={styles.userInfoStats}>
-                  <Icon
-                    name={Platform.OS === 'ios' ? 'ios-star' : 'md-star'}
-                    color="#555"
-                    size={20}
-                    style={styles.userInfoIcon}
-                  />
-                  <ShimmerPlaceHolder
-                    style={{ width: 30 }}
-                    visible={isFetched}
-                    backgroundColorBehindBorder={'#ddd'}
-                  >
-                    <Text style={styles.userInfoText}>{stars}</Text>
-                  </ShimmerPlaceHolder>
-                </View>
-                <View style={styles.userInfoStats}>
-                  <Icon
-                    name={Platform.OS === 'ios' ? 'ios-person' : 'md-person'}
-                    color="#555"
-                    size={20}
-                    style={styles.userInfoIcon}
-                  />
-                  <ShimmerPlaceHolder
-                    style={{ width: 30 }}
-                    visible={isFetched}
-                    backgroundColorBehindBorder={'#ddd'}
-                  >
-                    <Text style={styles.userInfoText}>{followers}</Text>
-                  </ShimmerPlaceHolder>
-                </View>
-              </View>
+            </View>
+            <View style={styles.userInfoStats}>
+              <Icon
+                name={Platform.OS === 'ios' ? 'ios-person' : 'md-person'}
+                color="#555"
+                size={20}
+                style={styles.userInfoIcon}
+              />
+              <ShimmerPlaceHolder
+                style={{ width: 30 }}
+                visible={isFetched}
+                backgroundColorBehindBorder={'#ddd'}
+              >
+                <Text style={styles.userInfoText}>{followers}</Text>
+              </ShimmerPlaceHolder>
             </View>
           </View>
-
-          <View style={styles.menuContainer}>
-            <TouchableOpacity style={styles.menuList}>
-              <Icon
-                name={Platform.OS === 'ios' ? 'ios-settings' : 'md-settings'}
-                color="#555"
-                size={25}
-                style={styles.icons}
-              />
-              <Text style={styles.menuText}>Ajustes</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.menuList}>
-              <Icon
-                name={
-                  Platform.OS === 'ios' ? 'ios-star-outline' : 'md-star-outline'
-                }
-                color="#555"
-                size={25}
-                style={styles.icons}
-              />
-              <Text style={styles.menuText}>Bee Points</Text>
-              <Text style={styles.menuTextPoints}>{points}</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.menuList}>
-              <Icon
-                name={
-                  Platform.OS === 'ios' ? 'ios-person-add' : 'md-person-add'
-                }
-                color="#555"
-                size={25}
-                style={styles.icons}
-              />
-              <Text style={styles.menuText}>Indicar Pessoas</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.menuList}>
-              <Icon
-                name={Platform.OS === 'ios' ? 'ios-card' : 'md-card'}
-                color="#555"
-                size={25}
-                style={styles.icons}
-              />
-              <Text style={styles.menuText}>Minhas Faturas</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.menuList}>
-              <Icon
-                name={Platform.OS === 'ios' ? 'ios-bicycle' : 'md-bicycle'}
-                color="#555"
-                size={25}
-                style={styles.icons}
-              />
-              <Text style={styles.menuText}>Auto Teste</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.menuList}>
-              <Icon
-                name={Platform.OS === 'ios' ? 'ios-lock' : 'md-lock'}
-                color="#555"
-                size={25}
-                style={styles.icons}
-              />
-              <Text style={styles.menuText}>Alterar Senha</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity
-              style={styles.menuList}
-              onPress={() => handleLogout()}
-            >
-              <Icon
-                name={Platform.OS === 'ios' ? 'ios-exit' : 'md-exit'}
-                color="#555"
-                size={25}
-                style={styles.icons}
-              />
-              <Text style={styles.menuText}>Sair</Text>
-            </TouchableOpacity>
-          </View>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      </View>
+
+      <View style={styles.menuContainer}>
+        <TouchableOpacity style={styles.menuList}>
+          <Icon
+            name={Platform.OS === 'ios' ? 'ios-settings' : 'md-settings'}
+            color="#555"
+            size={25}
+            style={styles.icons}
+          />
+          <Text style={styles.menuText}>Ajustes</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.menuList}>
+          <Icon
+            name={
+              Platform.OS === 'ios' ? 'ios-star-outline' : 'md-star-outline'
+            }
+            color="#555"
+            size={25}
+            style={styles.icons}
+          />
+          <Text style={styles.menuText}>Bee Points</Text>
+          <Text style={styles.menuTextPoints}>{points}</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.menuList}>
+          <Icon
+            name={Platform.OS === 'ios' ? 'ios-person-add' : 'md-person-add'}
+            color="#555"
+            size={25}
+            style={styles.icons}
+          />
+          <Text style={styles.menuText}>Indicar Pessoas</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.menuList}>
+          <Icon
+            name={Platform.OS === 'ios' ? 'ios-card' : 'md-card'}
+            color="#555"
+            size={25}
+            style={styles.icons}
+          />
+          <Text style={styles.menuText}>Minhas Faturas</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.menuList}>
+          <Icon
+            name={Platform.OS === 'ios' ? 'ios-bicycle' : 'md-bicycle'}
+            color="#555"
+            size={25}
+            style={styles.icons}
+          />
+          <Text style={styles.menuText}>Auto Teste</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.menuList}>
+          <Icon
+            name={Platform.OS === 'ios' ? 'ios-lock' : 'md-lock'}
+            color="#555"
+            size={25}
+            style={styles.icons}
+          />
+          <Text style={styles.menuText}>Alterar Senha</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.menuList}
+          onPress={() => handleLogout()}
+        >
+          <Icon
+            name={Platform.OS === 'ios' ? 'ios-exit' : 'md-exit'}
+            color="#555"
+            size={25}
+            style={styles.icons}
+          />
+          <Text style={styles.menuText}>Sair</Text>
+        </TouchableOpacity>
+      </View>
+    </MainContainer>
   )
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: '#202020' },
-
-  mainContainer: {
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    marginTop: -10,
-    backgroundColor: '#fff'
-  },
-
-  headerContainer: {
-    backgroundColor: '#202020',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 30,
-    paddingBottom: 20
-  },
-
-  headerText: {
-    fontWeight: '600',
-    fontSize: 22,
-    color: '#fff'
-  },
-
   userContainer: {
     alignItems: 'center',
     justifyContent: 'center',
