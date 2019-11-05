94189922f89baa3ae273bf7c2fa55f82714d4501
Mateus <mateus4k@protonmail.ch>
2019-11-05 00:08:12 -0300

    Redux Provider

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Provider } from 'react-redux'
 import Routes from './src/routes'
+
+import store from './src/store'
 
 import './src/config/reactotron'
 
-class App extends Component {
-  render() {
-    return <Routes />
-  }
-}
+const App = () => (
+  <Provider store={store}>
+    <Routes />
+  </Provider>
+)
 
 const OverlayApp = console.tron.overlay(App)
 
