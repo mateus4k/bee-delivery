e35ae1ec336465a9378ac5b4bab426b19f58ff32
Mateus <mateus4k@protonmail.ch>
2019-11-13 10:12:09 -0300

    Sample screen

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-import { View } from 'react-native'
+import { Image, StyleSheet, Dimensions } from 'react-native'
 
-// import { Container } from './styles';
+import Logo from '../assets/logo.png'
+import MainContainer from '../components/MainContainer'
 
-const Home = () => <View />
+const Home = () => (
+  <MainContainer name='Início'>
+    <Image source={Logo} style={styles.logo} resizeMode='center' />
+  </MainContainer>
+)
 
 export default Home
+
+const styles = StyleSheet.create({
+  logo: {
+    flex: 1,
+    width: 150,
+    alignSelf: 'center',
+    height: Dimensions.get('window').height - 100
+  }
+})
--- src/pages/Notification.js
+++ src/pages/Notification.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-import { View } from 'react-native'
+import { Image, StyleSheet, Dimensions } from 'react-native'
 
-// import { Container } from './styles';
+import Logo from '../assets/logo.png'
+import MainContainer from '../components/MainContainer'
 
-const Notification = () => <View />
+const Notification = () => (
+  <MainContainer name='Avisos'>
+    <Image source={Logo} style={styles.logo} resizeMode='center' />
+  </MainContainer>
+)
 
 export default Notification
+
+const styles = StyleSheet.create({
+  logo: {
+    flex: 1,
+    width: 150,
+    alignSelf: 'center',
+    height: Dimensions.get('window').height - 100
+  }
+})
