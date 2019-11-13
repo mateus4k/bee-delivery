33b893cdc8111fa897c732488d20fa611a010d4a
Mateus <mateus4k@protonmail.ch>
2019-11-13 09:10:49 -0300

    :bug: Lan config for Expo

--- src/config/reactotron.js
+++ src/config/reactotron.js
@@ -3,7 +3,7 @@ import { reactotronRedux } from 'reactotron-redux'
 import sagaPlugin from 'reactotron-redux-saga'
 
 if (process.env.NODE_ENV === 'development') {
-  const tron = Reactotron.configure({ host: '192.168.0.2' })
+  const tron = Reactotron.configure({ lan: 'exp://localhost:19000' })
     .use(reactotronRedux())
     .useReactNative()
     .use(sagaPlugin())
