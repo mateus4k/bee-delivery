80b1ebbf6ab479894766f673bc4bd3a2cb291ccf
Mateus <mateus4k@protonmail.ch>
2019-11-14 00:29:14 -0300

    :zap: Show user data from state

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { Component } from 'react'
 import {
   View,
   Text,
@@ -10,171 +10,182 @@ import {
 } from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons'
 import ShimmerPlaceHolder from 'react-native-shimmer-placeholder'
+
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { Creators as UserAction } from '../store/ducks/user'
+
 import MainContainer from '../components/MainContainer'
 
-export default function Profile({ navigation }) {
-  const [points, setPoints] = useState(0)
-  const [stars, setStars] = useState(0)
-  const [followers, setFollowers] = useState(0)
-  const [userImage, setUserImage] = useState('')
-  const [username, setUsername] = useState('')
-  const [isFetched, setIsFetched] = useState(false)
-
-  handleLogout = async () => {
-    await AsyncStorage.removeItem('@BeeDelivery:user')
-    navigation.navigate('Login')
+class Profile extends Component {
+  state = {
+    points: 0,
+    stars: 0,
+    followers: 0,
+    loading: true
   }
 
-  useEffect(() => {
-    setTimeout(() => {
-      setPoints(181.43)
-      setStars(4.13)
-      setFollowers(7)
-      setUserImage(
-        'https://avatars0.githubusercontent.com/u/30202634?s=460&v=4'
-      )
-      setUsername('Mateus Sampaio')
-      setIsFetched(true)
-    }, 1500)
-  }, [])
+  async handleLogout () {
+    await AsyncStorage.removeItem('@BeeDelivery:user')
+    this.props.navigation.navigate('Login')
+  }
 
-  return (
-    <MainContainer name="Perfil">
-      <View style={styles.userContainer}>
-        <ShimmerPlaceHolder
-          style={styles.userImage}
-          visible={isFetched}
-          backgroundColorBehindBorder={'#ddd'}
-        >
-          <Image
-            style={styles.userImage}
-            source={isFetched ? { uri: userImage } : null}
-          />
-        </ShimmerPlaceHolder>
+  componentDidMount () {
+    this.props.addUserRequest('mateus4k')
+    this.setState({ points: 181.43 })
+    this.setState({ stars: 4.13 })
+    this.setState({ followers: 7 })
+    this.setState({ loading: this.props.user.loading })
+  }
 
-        <View style={styles.userInfoContainer}>
+  render () {
+    const { points, stars, followers, loading } = this.state
+
+    return (
+      <MainContainer name='Perfil'>
+        <View style={styles.userContainer}>
           <ShimmerPlaceHolder
-            autoRun={true}
-            style={styles.userInfoName}
-            visible={isFetched}
-            backgroundColorBehindBorder={'#ddd'}
+            style={styles.userImage}
+            visible={!loading}
+            backgroundColorBehindBorder='#ddd'
           >
-            <Text style={styles.userInfoName}>{username}</Text>
+            {this.props.user.data.map(user => (
+              <Image
+                key={user.id}
+                style={styles.userImage}
+                source={!loading ? { uri: user.avatar } : null}
+              />
+            ))}
           </ShimmerPlaceHolder>
-          <View style={styles.userInfoList}>
-            <View style={styles.userInfoStats}>
-              <Icon
-                name={Platform.OS === 'ios' ? 'ios-star' : 'md-star'}
-                color="#555"
-                size={20}
-                style={styles.userInfoIcon}
-              />
-              <ShimmerPlaceHolder
-                style={{ width: 30 }}
-                visible={isFetched}
-                backgroundColorBehindBorder={'#ddd'}
-              >
-                <Text style={styles.userInfoText}>{stars}</Text>
-              </ShimmerPlaceHolder>
-            </View>
-            <View style={styles.userInfoStats}>
-              <Icon
-                name={Platform.OS === 'ios' ? 'ios-person' : 'md-person'}
-                color="#555"
-                size={20}
-                style={styles.userInfoIcon}
-              />
-              <ShimmerPlaceHolder
-                style={{ width: 30 }}
-                visible={isFetched}
-                backgroundColorBehindBorder={'#ddd'}
-              >
-                <Text style={styles.userInfoText}>{followers}</Text>
-              </ShimmerPlaceHolder>
+
+          <View style={styles.userInfoContainer}>
+            <ShimmerPlaceHolder
+              autoRun
+              style={styles.userInfoName}
+              visible={!loading}
+              backgroundColorBehindBorder='#ddd'
+            >
+              {this.props.user.data.map(user => (
+                <Text key={user.id} style={styles.userInfoName}>
+                  {user.name}
+                </Text>
+              ))}
+            </ShimmerPlaceHolder>
+            <View style={styles.userInfoList}>
+              <View style={styles.userInfoStats}>
+                <Icon
+                  name={Platform.OS === 'ios' ? 'ios-star' : 'md-star'}
+                  color='#555'
+                  size={20}
+                  style={styles.userInfoIcon}
+                />
+                <ShimmerPlaceHolder
+                  style={{ width: 30 }}
+                  visible={!loading}
+                  backgroundColorBehindBorder='#ddd'
+                >
+                  <Text style={styles.userInfoText}>{stars}</Text>
+                </ShimmerPlaceHolder>
+              </View>
+              <View style={styles.userInfoStats}>
+                <Icon
+                  name={Platform.OS === 'ios' ? 'ios-person' : 'md-person'}
+                  color='#555'
+                  size={20}
+                  style={styles.userInfoIcon}
+                />
+                <ShimmerPlaceHolder
+                  style={{ width: 30 }}
+                  visible={!loading}
+                  backgroundColorBehindBorder='#ddd'
+                >
+                  <Text style={styles.userInfoText}>{followers}</Text>
+                </ShimmerPlaceHolder>
+              </View>
             </View>
           </View>
         </View>
-      </View>
 
-      <View style={styles.menuContainer}>
-        <TouchableOpacity style={styles.menuList}>
-          <Icon
-            name={Platform.OS === 'ios' ? 'ios-settings' : 'md-settings'}
-            color="#555"
-            size={25}
-            style={styles.icons}
-          />
-          <Text style={styles.menuText}>Ajustes</Text>
-        </TouchableOpacity>
+        <View style={styles.menuContainer}>
+          <TouchableOpacity style={styles.menuList}>
+            <Icon
+              name={Platform.OS === 'ios' ? 'ios-settings' : 'md-settings'}
+              color='#555'
+              size={25}
+              style={styles.icons}
+            />
+            <Text style={styles.menuText}>Ajustes</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity style={styles.menuList}>
-          <Icon
-            name={
-              Platform.OS === 'ios' ? 'ios-star-outline' : 'md-star-outline'
-            }
-            color="#555"
-            size={25}
-            style={styles.icons}
-          />
-          <Text style={styles.menuText}>Bee Points</Text>
-          <Text style={styles.menuTextPoints}>{points}</Text>
-        </TouchableOpacity>
+          <TouchableOpacity style={styles.menuList}>
+            <Icon
+              name={
+                Platform.OS === 'ios' ? 'ios-star-outline' : 'md-star-outline'
+              }
+              color='#555'
+              size={25}
+              style={styles.icons}
+            />
+            <Text style={styles.menuText}>Bee Points</Text>
+            <Text style={styles.menuTextPoints}>{points}</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity style={styles.menuList}>
-          <Icon
-            name={Platform.OS === 'ios' ? 'ios-person-add' : 'md-person-add'}
-            color="#555"
-            size={25}
-            style={styles.icons}
-          />
-          <Text style={styles.menuText}>Indicar Pessoas</Text>
-        </TouchableOpacity>
+          <TouchableOpacity style={styles.menuList}>
+            <Icon
+              name={Platform.OS === 'ios' ? 'ios-person-add' : 'md-person-add'}
+              color='#555'
+              size={25}
+              style={styles.icons}
+            />
+            <Text style={styles.menuText}>Indicar Pessoas</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity style={styles.menuList}>
-          <Icon
-            name={Platform.OS === 'ios' ? 'ios-card' : 'md-card'}
-            color="#555"
-            size={25}
-            style={styles.icons}
-          />
-          <Text style={styles.menuText}>Minhas Faturas</Text>
-        </TouchableOpacity>
+          <TouchableOpacity style={styles.menuList}>
+            <Icon
+              name={Platform.OS === 'ios' ? 'ios-card' : 'md-card'}
+              color='#555'
+              size={25}
+              style={styles.icons}
+            />
+            <Text style={styles.menuText}>Minhas Faturas</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity style={styles.menuList}>
-          <Icon
-            name={Platform.OS === 'ios' ? 'ios-bicycle' : 'md-bicycle'}
-            color="#555"
-            size={25}
-            style={styles.icons}
-          />
-          <Text style={styles.menuText}>Auto Teste</Text>
-        </TouchableOpacity>
+          <TouchableOpacity style={styles.menuList}>
+            <Icon
+              name={Platform.OS === 'ios' ? 'ios-bicycle' : 'md-bicycle'}
+              color='#555'
+              size={25}
+              style={styles.icons}
+            />
+            <Text style={styles.menuText}>Auto Teste</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity style={styles.menuList}>
-          <Icon
-            name={Platform.OS === 'ios' ? 'ios-lock' : 'md-lock'}
-            color="#555"
-            size={25}
-            style={styles.icons}
-          />
-          <Text style={styles.menuText}>Alterar Senha</Text>
-        </TouchableOpacity>
+          <TouchableOpacity style={styles.menuList}>
+            <Icon
+              name={Platform.OS === 'ios' ? 'ios-lock' : 'md-lock'}
+              color='#555'
+              size={25}
+              style={styles.icons}
+            />
+            <Text style={styles.menuText}>Alterar Senha</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity
-          style={styles.menuList}
-          onPress={() => handleLogout()}
-        >
-          <Icon
-            name={Platform.OS === 'ios' ? 'ios-exit' : 'md-exit'}
-            color="#555"
-            size={25}
-            style={styles.icons}
-          />
-          <Text style={styles.menuText}>Sair</Text>
-        </TouchableOpacity>
-      </View>
-    </MainContainer>
-  )
+          <TouchableOpacity
+            style={styles.menuList}
+            onPress={() => this.handleLogout()}
+          >
+            <Icon
+              name={Platform.OS === 'ios' ? 'ios-exit' : 'md-exit'}
+              color='#555'
+              size={25}
+              style={styles.icons}
+            />
+            <Text style={styles.menuText}>Sair</Text>
+          </TouchableOpacity>
+        </View>
+      </MainContainer>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -256,3 +267,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   }
 })
+
+const mapStateToProps = state => ({
+  user: state.user
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators(UserAction, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile)
