b19438196afc88636cf560e4a1ccafd0b0375b65
Mateus <mateus4k@protonmail.ch>
2019-11-18 23:38:07 -0300

    Add Native Base

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import Routes from './src/routes'
+import * as Font from 'expo-font'
 
 import store from './src/store'
 
@@ -17,5 +18,10 @@ const App = () => (
 )
 
 const OverlayApp = console.tron.overlay(App)
+
+Font.loadAsync({
+  Roboto: require('native-base/Fonts/Roboto.ttf'),
+  Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf')
+})
 
 export default OverlayApp
